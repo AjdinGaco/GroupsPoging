e3cede66751ebb61415f2cd291453346ee2f5b5f
Ajdin <ajdin.gaco@student.ap.be>
2019-09-19 09:50:11 +0200

    Added my class for testing

---
+++ class1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupPoging
+{
+    class class1 
+    {
+        // Gemaakt door ajdin
+
+        public void show()
+        {
+            Console.WriteLine("Hi there,");
+            Console.WriteLine("General Kenobi");
+        }
+    }
+}
